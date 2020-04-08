4b879f70bf28f1a9517de8dffe50d3957b87bcc6
Andrew Tong <andrewtong713@gmail.com>
2020-04-08 16:50:12 -0500

    finished circle struct

--- As1/circle/circle.go
+++ As1/circle/circle.go
@@ -7,8 +7,26 @@ package circle
 
 import (
   "fmt"
+  "math"
 )
 
+
 type circle struct {
-  
+  x,y,radius float64
+}
+
+func (c circle) getArea() float64 {
+  return math.Pi*math.pow(c.radius,2)
+}
+
+func (c circle) getCircum() float64 {
+  return 2*math.Pi*c.radius
+}
+
+func (c circle) getVolume() float64 {
+  return 4*math.Pi*math.pow(c.radius,3)/3
+}
+
+func (c circle) getSurfaceA() float64 {
+  return 4*math.Pi*math.pow(c.radius,2)
 }
