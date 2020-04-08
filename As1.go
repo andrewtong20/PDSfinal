0aa32793a9964a158f5fcae20fc6d530f67a960c
Andrew Tong <andrewtong713@gmail.com>
2020-04-08 13:19:26 -0500

    added project introductions

--- As1.go
+++ As1.go
@@ -1,3 +1,14 @@
+/*
+Andrew Tong
+Mr. Tomczak
+4/13/20
+AS1
+
+This program finds the location, area, circumference, volume and surface area of the circle/sphere given a radius and x/y coordinates.
+It also asks the user for meters and converts it into the corresponding number of miles, feet, and inches. Then it prints the meters and the miles, feet, and inches to "meters.txt"
+Finally, it reads a line of text from the user, outputs the line with the first word moved to the end of the line, and prints the length of the string
+*/
+
 package main
 
 import "fmt"
--- As2.go
+++ As2.go
@@ -1,7 +1,23 @@
+/*
+Andrew Tong
+Mr. Tomczak
+4/20/20
+AS2
+
+This program gives the user a menu of options (1-5) which repeats until the user quits (option 5).
+Option 1 will print an investment table compounded daily given an initial investment, number of years, and range of integer interest rates.
+Option 2 will print a multiplication able given a range of starting and ending values.
+Option 3 will print all prime numbers to an inputted integer.
+Option 4 will convert an input seconds into days, hours, minutes, and seconds.
+ */
+
+
 package main
 
 import "fmt"
 
-func main() {
+func main()
+
+{
 	fmt.Printf("hello this is test two, one change later, update 2\n")
 }
